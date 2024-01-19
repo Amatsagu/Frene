0b6f122d49f9f179d8d021e21dd010c0a2fe5b19
Amatsagu <krzysztof.rapior@protonmail.com>
2024-01-19 22:14:01 +0100

    feat: temporarily disable unused code warning

--- src/command/scheme.rs
+++ src/command/scheme.rs
@@ -1,6 +1,8 @@
 use image::ImageError;
 use termcolor::StandardStream;
 
+#[allow(dead_code)]
+#[allow(unused_variables)]
 pub fn scheme_cmd_handler(stdout: &mut StandardStream, file_path: String) -> Result<(), ImageError> {
     Ok(())
 }
