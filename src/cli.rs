faee6b66d34f0df4215e847052fedc64525f0780
Amatsagu <krzysztof.rapior@protonmail.com>
2024-01-19 22:12:52 +0100

    feat: make strength param in blur command optional

--- src/cli.rs
+++ src/cli.rs
@@ -14,7 +14,7 @@ pub enum Commands {
     #[clap(arg_required_else_help = true)]
     Blur {
         file_path: String,
-        strength: f32
+        strength: Option<f32>
     },
     /// Analyze png/jpg image and create color-scheme based on most used colors in image
     #[clap(arg_required_else_help = true)]
